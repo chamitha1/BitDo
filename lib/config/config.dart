96c738366489b1fe44ddbdddb8ce3fa962935b63
Tharindu De Zoysa <tharindu.2015188@iit.ac.lk>
2026-01-02 11:25:25 +0530

    Live API

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -3,9 +3,12 @@ class AppConfig {
 
   static const bool isBuildApk = bool.fromEnvironment("dart.vm.product");
 
-  // static const String apiUrl = 'http://api.dev.bitdowallet.com/api';
+  // static const String apiUrl = 'http://api.dev.bitdowallet.com/api'; //dev
   // static const String webApiUrl = 'http://api.dev.bitdowallet.com/api';
 
-  static const String apiUrl = 'http://m-test.bitowi.com/api';
-  static const String webApiUrl = 'http://m-test.bitowi.com/api';
+  // static const String apiUrl = 'http://m-test.bitowi.com/api'; //test
+  // static const String webApiUrl = 'http://m-test.bitowi.com/api';
+
+  static const String apiUrl = 'http://m.bitowi.com/api'; //live
+  static const String webApiUrl = 'http://m.bitowi.com/api';
 }
